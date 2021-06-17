79b860e96a0b5334f450318e20d06025c7e23f95
dyotov87 <dyotov87@gmail.com>
2021-06-17 11:37:50 +0300

    fdfd
    fdfdfd

--- src/app/utility/app.init.ts
+++ src/app/utility/app.init.ts
@@ -4,16 +4,16 @@ import { KeycloakService } from "keycloak-angular";
 export function initializeKeycloak(keycloak: KeycloakService) : () => Promise<boolean> {
     return () =>
       keycloak.init({
-        // config: {
-        //   url: 'https://auth.eu.yuuvis.io/',
-        //   realm: 'itelligence1',
-        //   clientId: 'test-app',
-        // },
-        // initOptions: {
-        //     checkLoginIframe: true,
-        //     checkLoginIframeInterval: 25
-        // },
-        // loadUserProfileAtStartUp: true
+        config: {
+          url: 'https://auth.eu.yuuvis.io/auth/realms/itelligence1/protocol/openid-connect/auth?client_id=yuuvis-authentication-service&redirect_uri=https://eu.yuuvis.io/oauth/itelligence1&response_type=code&state=lmphK3',
+          realm: 'itelligence1',
+          clientId: 'test-app',
+        },
+        initOptions: {
+            checkLoginIframe: true,
+            checkLoginIframeInterval: 25
+        },
+        loadUserProfileAtStartUp: true
         // initOptions: {
         //   onLoad: 'check-sso',
         //   silentCheckSsoRedirectUri:
