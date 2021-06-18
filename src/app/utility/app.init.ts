67bae0c3280015f63c30db2e7043811882b8ec5c
dyotov87 <dyotov87@gmail.com>
2021-06-18 12:12:48 +0300

    fdsfsdsdsfd

--- src/app/utility/app.init.ts
+++ src/app/utility/app.init.ts
@@ -5,7 +5,7 @@ export function initializeKeycloak(keycloak: KeycloakService) : () => Promise<bo
     return () =>
       keycloak.init({
         config: {
-          url: 'https://eu.yuuvis.io',
+          url: 'https://auth.eu.yuuvis.io/auth/',
           realm: 'itelligence1',
           clientId: 'test-app',
         },
